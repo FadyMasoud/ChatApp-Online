613c35e4d13a661c110118ccb5a5f3ec9b0fc0a1
fady <fadykassed@gmail.com>
2025-12-15 22:20:47 +0200

    product-commit

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.Mongo_Connection_Production);
     console.log("True:**MongoDB Connected Successfully**");
   } catch (error) {
     console.error("False:### DB Connection Failed ###", error);
